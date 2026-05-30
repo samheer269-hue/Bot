61bee53ff383930c2c6772e1f0b6c583a1a15098
samheer269-hue <samheer269@gmail.com>
2026-05-30 15:13:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-
 import os
 from pyrogram import Client
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-SESSION_STRING = os.getenv("SESSION_STRING")
-
 app = Client(
     "userbot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    session_string=SESSION_STRING
+    api_id=int(os.getenv("API_ID")),
+    api_hash=os.getenv("API_HASH"),
+    session_string=os.getenv("SESSION_STRING")
 )
 
 app.run()
